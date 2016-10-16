22107c68c3135f04f94963baddc6dca43ff69998
Alexey Makhrov <amakhrov@inbox.ru>
2016-10-16 00:34:59 -0700

    Styling for the game field

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,16 +1,30 @@
 import React from 'react'
+import styles from './Field.scss'
 
-export const Field = (props) => (
-  <div style={{ margin: '0 auto' }} >
-    {props.rows.map((row, i) => (
-      <div key={i}>
+export const Field = (props) => {
+  const {rows} = props
+  const cellsInRow = rows[0].length
+  const size = (100 / cellsInRow) + '%'
+
+  return <div style={{ margin: '0 auto' }} >
+    {rows.map((row, i) => (
+      <div key={i} className={styles.row}>
         {row.map((symbol, j) => (
-          <span key={j} onClick={() => props.selectCell(i, j)}>{symbol}</span>
+          <span
+            key={j}
+            className={styles.cell}
+            onClick={() => props.selectCell(i, j)}
+            data-symbol={symbol}
+            style={{
+              width: size,
+              paddingBottom: size
+            }}
+          />
         ))}
       </div>
     ))}
   </div>
-)
+}
 
 Field.propTypes = {
   rows: React.PropTypes.arrayOf(React.PropTypes.array).isRequired,
