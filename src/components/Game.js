387dc52f34811e7a069620b250cbeec1276be3b2
Alexey Makhrov <amakhrov@inbox.ru>
2016-10-16 11:24:52 -0700

    More pacing between game control buttons

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Field from './Field'
 import Status from './Status'
+import styles from './Game.scss'
+
+const Button = (props) =>  (
+  <button {...props} className={`btn btn-default ${styles.button}`}>{props.children}</button>
+)
 
 export const Game = (props) => {
   return   <div style={{ margin: '0 auto' }} >
     <Status {...props.state} />
-    <button className='btn btn-default' onClick={props.start}>Restart</button>
-    <button className='btn btn-default' onClick={props.undo} disabled={!props.canUndo}>Undo</button>
-    <button className='btn btn-default' onClick={props.redo} disabled={!props.canRedo}>Redo</button>
+    <Button onClick={props.start}>Restart</Button>
+    <Button onClick={props.undo} disabled={!props.canUndo}>Undo</Button>
+    <Button onClick={props.redo} disabled={!props.canRedo}>Redo</Button>
     <Field rows={props.state.rows} winningCells={props.state.winningCells} selectCell={props.selectCell}/>
   </div>
 
