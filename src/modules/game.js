fdddd4ddc6ef3ec00ca5e4b5c0836b90372d5c05
Alexey Makhrov <amakhrov@inbox.ru>
2017-02-05 18:56:54 -0800

    Change default player names to Player 1/2

--- src/modules/game.js
+++ src/modules/game.js
@@ -59,8 +59,8 @@ const ACTION_HANDLERS = {
 
 export const initialState = resetGame({
   players: [
-    {name: 'Pasha', symbol: SYMBOL_O},
-    {name: 'Anti', symbol: SYMBOL_X},
+    {name: 'Player 1', symbol: SYMBOL_O},
+    {name: 'Player 2', symbol: SYMBOL_X},
   ],
   fieldSize: 20,
   lineSizeToWin: 5,
